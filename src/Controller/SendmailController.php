1e00e4cfbf1e6495fbe1a93fc1e539c49888cde2
Burak Aydin <aydin@eonm.de>
2017-11-30 10:55:14 +0100

    initial commit

---
+++ src/Controller/SendmailController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Eonm\Sendmail\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SendmailController extends Controller
+{
+    public function sendAction()
+    {
+        die('ddd');
+    }
+}
---
+++ src/Sendmail.php
@@ -0,0 +1,8 @@
+<?php
+namespace Eonm\Sendmail;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Sendmail extends Bundle
+{
+}
